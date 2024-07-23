758c456fd8dd8cb839db8442e7514bc82833931e
Nxele <sizwenduduzow@gmail.com>
2024-07-23 21:59:37 +0200

    added a second get API with a mapping

--- src/main/kotlin/com/vecter/tutorial/springboot/vecter/HelloWorldController.kt
+++ src/main/kotlin/com/vecter/tutorial/springboot/vecter/HelloWorldController.kt
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("api/hello")
 class HelloWorldController {
-    @GetMapping
-    fun helloWord() : String{
+    @GetMapping("/hello")
+    fun helloWord(): String {
         return "Hello vecter king welcome to spring boot"
+    }
+
+    @GetMapping("/name")
+    fun getName(): String {
+        return "Nduduzo Sizwe Vecter Boss BMV NXele  \uD83D\uDE01"
     }
 }
