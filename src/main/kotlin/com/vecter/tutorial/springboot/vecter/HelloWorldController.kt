f816b115fd78de42f9ab4c446431ed2b2036c0f9
Nxele <sizwenduduzow@gmail.com>
2024-08-01 23:52:29 +0200

    Added a service and a API for returning all the banks account

--- src/main/kotlin/com/vecter/tutorial/springboot/vecter/HelloWorldController.kt
+++ src/main/kotlin/com/vecter/tutorial/springboot/vecter/HelloWorldController.kt
@@ -1,11 +1,13 @@
 package com.vecter.tutorial.springboot.vecter
 
+import com.vecter.tutorial.springboot.vecter.dataSource.mock.mockBankDataSource
+import com.vecter.tutorial.springboot.vecter.model.Bank
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("api/hello")
+@RequestMapping("api/banks")
 class HelloWorldController {
     @GetMapping("/world")
     fun helloWord(): String {
@@ -15,5 +17,11 @@ class HelloWorldController {
     @GetMapping("/name")
     fun getName(): String {
         return "Nduduzo Sizwe Vecter Boss BMV NXele \uD83D\uDE01"
+    }
+
+    @GetMapping("/banker")
+    fun getBanker() : Collection<Bank>{
+        val bankers = mockBankDataSource().retrieveBanks()
+        return bankers
     }
 }
---
+++ src/main/kotlin/com/vecter/tutorial/springboot/vecter/service/Bankservice.kt
@@ -0,0 +1,8 @@
+package com.vecter.tutorial.springboot.vecter.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class Bankservice {
+
+}
